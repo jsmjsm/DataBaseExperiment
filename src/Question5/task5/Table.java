d64002d02d5621bd82316b80bb7c25bb13ca9bcc
levy pan <levy-pan@outlook.com>
2020-11-26 16:27:28 +0800

    task 5

---
+++ src/Question5/task5/Table.java
@@ -0,0 +1,71 @@
+package task5;
+
+import java.awt.*;
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.table.TableColumn;
+
+// Build the table
+public class Table {
+    private String[][] Data = new String[200][14];
+    private String[] tableTitle = new String[] { "EMPNO", "FIRSTNME", "MIDINIT", "LASTNAME", "WORKDEPT", "PHONENO",
+            "HIREDATE", "JOB", "EDLEVEL", "SEX", "BIRTHDATE", "SALARY", "BONUS", "COMM" };
+    private JTable TheTable;
+
+    // MARK: Init
+    Table() throws SQLException {
+        int row = 0;
+        // TODO: Get connection 的方式可能有变化
+        Connection connection = DriverManager.getConnection("jdbc:db2:sample");
+        connection.setAutoCommit(false);
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT EMPNO, FIRSTNME, MIDINIT, LASTNAME, WORKDEPT, PHONENO, HIREDATE, "
+                + "JOB, EDLEVEL, SEX, BIRTHDATE, SALARY, BONUS, COMM" + " FROM EMPLOYEE ");
+        while (rs.next()) {
+            for (int i = 0; i < 14; i++) {
+                Data[row][i] = rs.getString(i + 1);
+            }
+            row++;
+        }
+        rs.close();
+        stmt.close();
+        // MARK Table
+        TableColumn column;
+        this.TheTable = new JTable(Data, tableTitle);
+        TheTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        // 设置列宽
+        for (int i = 0; i < 14; i++) {
+            column = TheTable.getColumnModel().getColumn(i);
+            column.setPreferredWidth(90);
+        }
+    }
+
+    // 返回表格
+    JTable getTheTable() {
+        return this.TheTable;
+    }
+
+    // refresh data
+    void refreshData() {
+        try {
+            int row = 0;
+            // TODO: Get connection 的方式可能有变化
+            Connection connection = DriverManager.getConnection("jdbc:db2:sample");
+            connection.setAutoCommit(false);
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT EMPNO, FIRSTNME, MIDINIT, LASTNAME, WORKDEPT, PHONENO, HIREDATE, "
+                    + "JOB, EDLEVEL, SEX, BIRTHDATE, SALARY, BONUS, COMM" + " FROM EMPLOYEE ");
+            while (rs.next()) {
+                for (int i = 0; i < 14; i++) {
+                    Data[row][i] = rs.getString(i + 1);
+                }
+                row++;
+            }
+            rs.close();
+            stmt.close();
+            TheTable.updateUI(); // updateUI
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/task5/README.md
@@ -0,0 +1,7 @@
+# Task 5
+
+编译
+`javac -d . *.java`
+
+运行
+`java task5.Task5`
